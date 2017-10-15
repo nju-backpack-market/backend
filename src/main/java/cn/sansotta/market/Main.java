c90b5fc9c721c7cd50e125ac2fc53ac42ad4109d
tinker <tinker19981@hotmail.com>
2017-10-15 16:50:23 +0800

    code: 改下目录名字，换回java

---
+++ src/main/java/cn/sansotta/market/Main.java
@@ -0,0 +1,14 @@
+package cn.sansotta.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="mailto:tinker19981@hotmail.com">tinker</a>
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String... args) {
+        SpringApplication.run(Main.class);
+    }
+}
