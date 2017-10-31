af9de04dc1f574d9405336f8c3416194e9a51ad5
aneureka <aneureka2@gmail.com>
2017-11-01 00:26:20 +0800

    code: modify/add mappers(simple test passed), dao...

--- src/main/java/cn/sansotta/market/Main.java
+++ src/main/java/cn/sansotta/market/Main.java
@@ -1,5 +1,10 @@
 package cn.sansotta.market;
 
+import cn.sansotta.market.domain.entity.*;
+import cn.sansotta.market.domain.value.OrderState;
+import cn.sansotta.market.domain.value.ShoppingItem;
+import cn.sansotta.market.mapper.ProductMapper;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,12 +14,36 @@ import cn.sansotta.market.common.MybatisUtils;
 import cn.sansotta.market.mapper.OrderMapper;
 import cn.sansotta.market.mapper.ShoppingItemMapper;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author <a href="mailto:tinker19981@hotmail.com">tinker</a>
  */
 @SpringBootApplication
-public class Main {
+public class Main implements CommandLineRunner  {
     public static void main(String... args) {
         SpringApplication.run(Main.class);
     }
+
+    final private OrderMapper mapper;
+
+    public Main(OrderMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        OrderEntity order = new OrderEntity(2, Double.valueOf(1111), OrderState.CREATE, LocalDateTime.now(), new DeliveryInfoEntity("hiki", "110", "email", "nj"), new ArrayList<>());
+        this.mapper.updateOrder(order);
+
+//        System.out.println(this.mapper.selectShoppingItemsByOrderId(1));
+//        List<ShoppingItemEntity> list = new ArrayList<>();
+//        list.add(new ShoppingItemEntity(1, 222, 1, new PriceEntity(1, Double.valueOf(1)), Double.valueOf(1) ));
+//        this.mapper.insertShoppingItems(list);
+//        this.mapper.deleteShoppingItems(1);
+
+    }
+
 }
